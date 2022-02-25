ba552c0a7912f6a5afb82a1274c27e2bacc263ab
David Shoemaker <DaShoe@microsoft.com>
2022-02-25 14:00:30 -0800

    update so can move to edge repo for validation testing

---
+++ Main.cpp
@@ -0,0 +1,213 @@
+
+#include <windows.h>
+#include <string>
+#include <wrl.h>
+#include <algorithm>
+#include <vector>
+#include "WebView2.h"
+#include "WebView2EnvironmentOptions.h"
+
+using namespace Microsoft::WRL;
+
+static wchar_t szWindowClass[] = L"DesktopApp";
+static wchar_t szTitle[] = L"WebView sample";
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void CreateWebView();
+
+HINSTANCE hInst;
+std::vector<ComPtr<ICoreWebView2Controller>> m_webviewWindows;
+HWND m_hwndMain;
+std::wstring m_exe_path;
+std::wstring m_url;
+HMENU IDE_CREATE = (HMENU)102;
+DWORD m_webviewTop = 25;
+
+int CALLBACK WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nCmdShow)
+{
+    // Create the desktop window class
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex))
+    {
+        return E_FAIL;
+    }
+
+    hInst = hInstance;
+
+    // Create the window
+    m_hwndMain = CreateWindow(
+        szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        1100, 200,
+        1200, 900,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    if (!m_hwndMain)
+    {
+        return E_FAIL;
+    }
+
+    // Show the window
+    ShowWindow(m_hwndMain, nCmdShow);
+    UpdateWindow(m_hwndMain);
+
+    // Get the url that we want to display in the webview from our current path
+    wchar_t system_buffer[MAX_PATH];
+    system_buffer[0] = 0;
+    DWORD len = ::GetModuleFileName(hInstance, system_buffer, MAX_PATH);
+    if (len == 0 || len > MAX_PATH)
+        return E_FAIL;
+    std::wstring fullPath(system_buffer, len);
+    size_t found = fullPath.find_last_of(L"/\\");
+    std::wstring path = fullPath.substr(0, found);
+    m_exe_path = path;
+    std::replace(path.begin(), path.end(), L'\\', L'/');
+    std::wstring url(L"file:///");
+    url += path;
+    url += L"/index.html";
+    m_url = url;
+    OutputDebugString(L"\r\n");
+    OutputDebugString(m_url.c_str());
+    OutputDebugString(L"\r\n");
+
+    SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+    // Create a single WebView within the parent window
+    CreateWebView();
+
+    // Main message loop
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+void LayoutWebViews()
+{
+    RECT bounds;
+    GetClientRect(m_hwndMain, &bounds);
+
+    size_t s = m_webviewWindows.size();
+    LONG w = bounds.right - bounds.left;
+
+    size_t width = (w / s) - (s - 1);
+
+    for (size_t i = 0; i < m_webviewWindows.size(); i++)
+    {
+        RECT win = bounds;
+        win.top = m_webviewTop;
+        win.left = (LONG)(i * width + i * 2);
+        win.right = (LONG)(i * width + width);
+        m_webviewWindows[i]->put_Bounds(win);
+    }
+}
+
+void CreateWebView()
+{
+    HRESULT hr  E_FAIL;
+
+    bool debugPortOn = true;
+    auto options = Microsoft::WRL::Make<CoreWebView2EnvironmentOptions>();
+    if (debugPortOn)
+    {
+        options->put_AdditionalBrowserArguments(L"--remote-debugging-port=9222");
+    }
+
+    hr = CreateCoreWebView2EnvironmentWithOptions(
+        L"",    // Default runtime location
+        L"",    // Default User Data Folder
+        options.Get(),                          // Options
+        Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+            [&](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+                // Create a WebView, whose parent is the main window hWnd
+                result = env->CreateCoreWebView2Controller(m_hwndMain, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+                    [&](HRESULT result, ICoreWebView2Controller* webview) -> HRESULT {
+                        if (webview == nullptr)
+                        {
+                            result = E_FAIL;
+                        }
+                        else
+                        {
+                            m_webviewWindows.push_back(webview);
+
+                            // Add a few settings for the webview
+                            ICoreWebView2* cwv2 = nullptr;
+                            ICoreWebView2Settings* Settings;
+
+                            webview->get_CoreWebView2(&cwv2);
+                            cwv2->get_Settings(&Settings);
+                            Settings->put_IsScriptEnabled(TRUE);
+                            Settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+                            Settings->put_IsWebMessageEnabled(TRUE);
+
+                            // Resize WebView to fit the bounds of the parent window
+                            LayoutWebViews();
+
+                            // Load our html content
+                            std::wstring url = m_url;
+                            url.append(L"?i=");
+                            url.append(std::to_wstring(m_webviewWindows.size() - 1));
+
+                            result = cwv2->Navigate(url.c_str());
+                        }
+                        return result;
+                    })
+                    .Get());
+                return result;
+            })
+        .Get());
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_SIZE:
+        if (!m_webviewWindows.empty())
+        {
+            LayoutWebViews();
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == LOWORD(IDE_CREATE))
+        {
+            CreateWebView();
+        }
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+        break;
+    }
+
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Setup
-This is a test bed application for exercising the two javascript debugger plugins.  This is a dev tool and not designed for public release due to hardcover paths and dependencies.
+This is a test bed application for exercising the legacy javascript debugger plugin for Visual Sudio 2019 / 2022.  This is a dev tool and not designed for public release due to hardcover paths and dependencies.
 
 ## Visual Studio setup
     Load the webviewexample.sln file in Visual Studio
@@ -8,30 +8,6 @@ This is a test bed application for exercising the two javascript debugger plugin
 
     Build the webviewexample.exe
 
-    You are now down with Visual Studio
 ## Missing DLL Depencency
 Copy icuuc.dll from your ana\src\out\debug_x64 directory to webviewexample\out\debug\x64\webviewexample this will fix a runtime dependency error resulting in a "webview2loader.dll was not found error".
 
-## Visual Code setup
-### Update the launch.json
-"runtimeExecutable" change this to the path of the webviewexaple.exe you just built
-"path" update the path to match your build envrionment;
-    IE: "Path": "%path%;e://ana//src//out//debug_x64;E:/github/WebViewExample/out/Debug/x64/WebViewExample/WebViewExample"
-
-### Install Debugger add ins
- Install the old debug adapter: "Debugger for Microsoft Edge"
- Install the new debug adapter: "JavaScript Debugger" 
-
-## Usage
-Scenario 0 is c++ debugging of the test application
-Scenarios 1-3 are using the old debug adapter
-Scenarios 4-6 are using the new debug adapter
-### Scenario 1
-    To Test basic debugger functionality of the old debugger add in.  This will attach to the first webview created.
-### Scenario 2
-    To test targeted webview debugger functionality.  This will attach to the first webview that navigates to a URL that matches the value set in "UrlFilter".
-### Scenario 3
-    To test basic debugger functionality of the new debugger add in.  This will attach to the first webview created
-### Scenario 4
-    To test targeted webview debugger functionality.  This will attach to the first webview that navigates to a URL that matches the value set in "UrlFilter".
-    NOTE: Currently not working
